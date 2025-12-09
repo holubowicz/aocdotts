d872e216dcd7f625d906cc8de6839bd26859f87d
Tymoteusz Hołubowicz <103210917+holubowicz@users.noreply.github.com>
2025-12-09 20:50:58 +0100

    aoc: day eight

---
+++ solutions/2025/day08/partOne.ts
@@ -0,0 +1,49 @@
+import { run } from "aoc-framework";
+
+type Coord = [number, number, number];
+
+interface Pair {
+	a: Coord;
+	b: Coord;
+	distance: number;
+}
+
+function calculateDistance(a: Coord, b: Coord) {
+	return Math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2);
+}
+
+run(
+	(input, { test }) => {
+		const PAIRS_CUTOFF = test ? 10 : 1000;
+
+		const coords = input.map((line) => line.split(",").map(Number)) as Coord[];
+		const pairs: Pair[] = coords
+			.flatMap((a, i) => coords.slice(i + 1).map((b) => ({ a, b, distance: calculateDistance(a, b) })))
+			.sort((a, b) => a.distance - b.distance)
+			.slice(0, PAIRS_CUTOFF);
+
+		const circuits: Coord[][] = [];
+		for (const { a, b } of pairs) {
+			const circuitA = circuits.find((c) => c.includes(a));
+			const circuitB = circuits.find((c) => c.includes(b));
+
+			if (!circuitA && !circuitB) {
+				circuits.push([a, b]);
+			} else if (circuitA && !circuitB) {
+				circuitA.push(b);
+			} else if (!circuitA && circuitB) {
+				circuitB.push(a);
+			} else if (circuitA && circuitB && circuitA !== circuitB) {
+				circuitA.push(...circuitB);
+				circuits.splice(circuits.indexOf(circuitB), 1);
+			}
+		}
+
+		return circuits
+			.map((c) => c.length)
+			.sort((a, b) => b - a)
+			.slice(0, 3)
+			.reduce((prev, curr) => prev * curr, 1);
+	},
+	{ inputMode: "lines" },
+);
